7bdc6ab95ac1584fcc2dfa411ec1774c8dc21ed8
Janki Gandhi <jankigandhi@atyantik.com>
2018-08-14 22:33:16 +0530

    setup programming school

---
+++ inc/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Elements</title>
+		<meta name="description" content="elements">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="../../images/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:100,200,300,400,500,600,700" />
+		<link rel="stylesheet" href="../css/libs/fontawesome.min.css">
+		<link rel="stylesheet" href="../css/style.css">
+	</head>
+   
+	
---
+++ pages/home.php
@@ -0,0 +1,5 @@
+<?php include_once '../inc/head.php';?>
+<body>
+  <h1>Hello</h1>
+</body>
+</html>
