0c20d7bdf97dc7e751f7f9533c078e02b4148568
Janki Gandhi <jankigandhi@atyantik.com>
2018-08-16 11:17:59 +0530

    designing home page

--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,122 @@
   <?php include_once '../inc/header.php';?>
   <!-- end header -->
   <!-- site-content -->
-  <main></main>
+  <main>
+    <div>
+      <div class="container">
+        <div class="row py-4">
+          <div class="col-lg-3 col-sm-6">
+            <div class="blog-box">
+              <div class="blog-img">
+                <a class="bg-img" style="background-image: url('images/blog1.png')"></a>
+              </div>
+              <div class="blog-content mt-2">
+                <h6>
+                  <a href="#" class="gray-600">Sandwich with delicious cheeses and Tasty Toppings</a>
+                </h6>
+                <div class="mb-2">
+                  <span class="small gray-500">
+                    <i class="fas fa-user pr-1"></i>
+                    Templates Yard
+                  </span>
+                  <span class="small gray-500">
+                    <i class="far fa-calendar pr-1"></i>
+                    Mar 17, 2016
+                  </span>
+                </div>
+                <div>
+                  <p class="mb-0">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="blog-box">
+              <div class="blog-img">
+                <a class="bg-img" style="background-image: url('images/blog1.png')"></a>
+              </div>
+              <div class="blog-content mt-2">
+                <h6>
+                  <a href="#" class="gray-600">Sandwich with delicious cheeses and Tasty Toppings</a>
+                </h6>
+                <div class="mb-2">
+                  <span class="small gray-500">
+                    <i class="fas fa-user pr-1"></i>
+                    Templates Yard
+                  </span>
+                  <span class="small gray-500">
+                    <i class="far fa-calendar pr-1"></i>
+                    Mar 17, 2016
+                  </span>
+                </div>
+                <div>
+                  <p class="mb-0">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="blog-box">
+              <div class="blog-img">
+                <a class="bg-img" style="background-image: url('images/blog1.png')"></a>
+              </div>
+              <div class="blog-content mt-2">
+                <h6>
+                  <a href="#" class="gray-600">Sandwich with delicious cheeses and Tasty Toppings</a>
+                </h6>
+                <div class="mb-2">
+                  <span class="small gray-500">
+                    <i class="fas fa-user pr-1"></i>
+                    Templates Yard
+                  </span>
+                  <span class="small gray-500">
+                    <i class="far fa-calendar pr-1"></i>
+                    Mar 17, 2016
+                  </span>
+                </div>
+                <div>
+                  <p class="mb-0">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="blog-box">
+              <div class="blog-img">
+                <a class="bg-img" style="background-image: url('images/blog1.png')"></a>
+              </div>
+              <div class="blog-content mt-2">
+                <h6>
+                  <a href="#" class="gray-600">Sandwich with delicious cheeses and Tasty Toppings</a>
+                </h6>
+                <div class="mb-2">
+                  <span class="small gray-500">
+                    <i class="fas fa-user pr-1"></i>
+                    Templates Yard
+                  </span>
+                  <span class="small gray-500">
+                    <i class="far fa-calendar pr-1"></i>
+                    Mar 17, 2016
+                  </span>
+                </div>
+                <div>
+                  <p class="mb-0">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
   <!-- end site-content -->
   <!-- footer -->
   <?php include_once '../inc/footer.php';?>
