f92c0505be210aff083db820a202fe675e2738c1
Janki Gandhi <jankigandhi@atyantik.com>
2018-10-13 17:01:59 +0530

    css changes

--- inc/head.php
+++ inc/head.php
@@ -7,7 +7,8 @@
 		<meta name="description" content="ProgrammingSchool">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="../images/fav.png">
-		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:100,200,300,400,500,600,700" />
+		<!-- <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:100,200,300,400,500,600,700" /> -->
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Nunito:100,400,300,500,600,700" />
 		<link rel="stylesheet" href="../css/libs/fontawesome.min.css">
 		<link rel="stylesheet" href="../css/style.css">
 	</head>
